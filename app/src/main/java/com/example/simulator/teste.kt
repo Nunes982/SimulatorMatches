79045c31ab77cdfd42c4af5a45643a5606084259
Nunes982 <ansso06061982@gmail.com>
2022-06-18 16:30:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/simulator/teste.kt
@@ -0,0 +1,13 @@
+package com.example.simulator
+
+class teste {
+    private fun getFullName(name:String, lastName:String):String{
+        val fullName = "$name $lastName"
+        return fullName
+    }
+    private fun getNameComp(name:String, lastName:String):String{
+        return "$name $lastName"
+
+    }
+    private fun nomecompleto(nome:String, sobreNome:String) = "$nome $sobreNome"
+}
